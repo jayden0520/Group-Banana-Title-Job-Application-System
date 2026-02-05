089c09458592510e84c9ab9cbce784d0c5a41a61
jayden0520 <jaydenlow88888@gmail.com>
2026-02-05 20:58:21 +0800

    Delete project directory

--- project/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "job_application_system");
-
-if (!$conn) {
-    die("Connection failed");
-}
-?>
--- project/job.js
+++
@@ -1,8 +0,0 @@
-function validateApplyForm() {
-    const cover = document.getElementById("cover").value;
-    if (cover.trim() === "") {
-        alert("Cover letter cannot be empty");
-        return false;
-    }
-    return true;
-}
